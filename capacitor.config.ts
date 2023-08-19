1e866f80e1dbedfbc37984f2f82e2b0c9fde07ea
Nur Ridho A <abdurrahmansyah234@gmail.com>
2023-08-19 11:18:08 +0700

    Push First Code

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ridhtech.medsmanual',
+  appName: 'Meds Manual',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
